db50757d44266b4e4aa95e4719303ba0e5b92e5c
Markkex <avila.marco.dev@gmail.com>
2023-09-17 23:30:29 +0100

    implement GET

---
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -0,0 +1,6 @@
+package example.cashcard;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
